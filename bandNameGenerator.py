824787675acbe2a84f6804ed4640f3839a1c65e1
Prashant Gupta <93766389+guptaprashant4@users.noreply.github.com>
2024-02-02 02:12:37 -0600

    bandNameGenerator.py
    1st day of python
    Project 1

---
+++ bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_born_in = input("What's the name of the city you were born in?\n-->")
+pet_name = input("What's your pet's name?\n-->")
+print("Your band name could be " + city_born_in + " " + pet_name)
