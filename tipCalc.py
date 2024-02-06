ec6d721593a76045b1970a6f9a4c8c4ed04945fd
Prashant Gupta <93766389+guptaprashant4@users.noreply.github.com>
2024-02-06 10:37:58 -0600

    Errors corrected

--- tipCalc.py
+++ tipCalc.py
@@ -5,6 +5,6 @@ num_people = int(input("How many people to split the bill? "))
 tip_percentage = int(input("What percentage of tip would you like to give? 10, 12 or 15? "))
 
 bill_w_tip = total_bill * (tip_percentage/100) + total_bill
-amnt_per_person = bill_w_tip / num_people
+amnt_per_person = round((bill_w_tip / num_people), 2)
 
-print("Each person should pay: " + amnt_per_person)
+print("Each person should pay: $" + str(amnt_per_person))
